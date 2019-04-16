9ad1b221692dfc0281a825860f3e44f7380125e0
Divya Sasidharan <hello@shortdiv.com>
2019-04-16 12:51:18 -0400

    fix: assets

--- src/main.js
+++ src/main.js
@@ -2,13 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import ApolloClient from "apollo-boost"
 Vue.config.productionTip = false
-
-const apolloProvider = new ApolloClient({
-  // You should use an absolute URL here
-  uri: "/.netlify/functions/fauna-graphql"
-})
 
 new Vue({
   store,
