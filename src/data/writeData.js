8b79b23d4c5cc0f5a79bf101b6065f86da0c22c0
Divya Sasidharan <hello@shortdiv.com>
2019-04-16 12:30:19 -0400

    delete data

--- src/data/writeData.js
+++
@@ -1,9 +0,0 @@
-var csv2json = require('csv2json');
-var fs = require('fs');
-
-fs.createReadStream('./src/data/marathon_results_2017.csv')
-  .pipe(csv2json({
-    // Defaults to comma.
-    // separator: ';'
-  }))
-  .pipe(fs.createWriteStream('./src/data/data.json'));
